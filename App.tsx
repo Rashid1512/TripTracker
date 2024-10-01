eef49bf609cf460951b9e04d1bc5600eefd7079a
Rashid <116960022+Rashid1512@users.noreply.github.com>
2024-10-01 19:51:27 +0500

    GeoLoc Pkg, Switch

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 
 import 'react-native-gesture-handler';
-import React from 'react';
-import { Button, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, View, Switch } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
-
+import Geolocation from '@react-native-community/geolocation';
+// Geolocation.setRNConfiguration(config);
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -17,7 +18,8 @@ function HomeScreen() {
 function NotificationsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Go back home" />
+      <Button onPress={()=>{Geolocation.getCurrentPosition(info => console.log(info))
+}} title="Go back home" />
     </View>
   );
 }
@@ -25,11 +27,21 @@ function NotificationsScreen() {
 
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Home" component={HomeScreen} 
+        options={{ headerRight: () => (
+          <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+        ),}}/>
+        <Drawer.Screen name="About" component={NotificationsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
