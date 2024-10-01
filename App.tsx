4b462dac62801588365e5ea2088ad2f5cbb90ff4
Rashid <116960022+Rashid1512@users.noreply.github.com>
2024-10-01 17:08:39 +0500

    Drawer Installed

--- App.tsx
+++ App.tsx
@@ -1,23 +1,43 @@
+
+import 'react-native-gesture-handler';
 import React from 'react';
+import { Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+const Drawer = createDrawerNavigator();
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      
+    </View>
+  );
+}
 
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
-function App()
-{
-
-  
-  return(<NavigationContainer><View></View></NavigationContainer>)
+function NotificationsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Go back home" />
+    </View>
+  );
 }
 
 
-const styles = StyleSheet.create({
-  
-});
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+// const styles = StyleSheet.create({ 
+// });
+
