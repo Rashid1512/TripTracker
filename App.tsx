9c0d28e3c5aa7ef3d20d734ce96961943e1c1391
Rashid <116960022+Rashid1512@users.noreply.github.com>
2024-10-03 14:07:05 +0500

    Header Screen Dark Mode

--- App.tsx
+++ App.tsx
@@ -1,55 +1,79 @@
-
 import 'react-native-gesture-handler';
 import React, {useState} from 'react';
-import { Button, View, Switch } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {Button, View, Switch} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
 import Geolocation from '@react-native-community/geolocation';
 // Geolocation.setRNConfiguration(config);
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      
-    </View>
-  );
-}
-
-function NotificationsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={()=>{Geolocation.getCurrentPosition(info => console.log(info))
-}} title="Go back home" />
-    </View>
-  );
-}
-
-
 
 export default function App() {
+  function HomeScreen() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: isEnabled ? 'black' : 'white',
+        }}></View>
+    );
+  }
+  function AboutScreen() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}></View>
+    );
+  }
+  function SettingScreen() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}></View>
+    );
+  }
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} 
-        options={{ headerRight: () => (
-          <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-        ),}}/>
-        <Drawer.Screen name="About" component={NotificationsScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: isEnabled ? 'darkgray' : 'white',
+          },
+        }}
+        initialRouteName="Home">
+        <Drawer.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerTintColor: isEnabled ? 'white' : 'black',
+            headerStyle: {
+              backgroundColor: isEnabled ? 'black' : 'white',
+            },
+            headerRight: () => (
+              <Switch
+                trackColor={{false: '#767577', true: '#81b0ff'}}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Setting" component={SettingScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
-
-
-
-// const styles = StyleSheet.create({ 
+// const styles = StyleSheet.create({
 // });
-
