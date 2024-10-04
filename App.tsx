d01899c620fea2aa9ee947d166e16173cd2aed33
Rashid <116960022+Rashid1512@users.noreply.github.com>
2024-10-04 17:44:13 +0500

    Balance, Bundle details UI

--- App.tsx
+++ App.tsx
@@ -1,22 +1,188 @@
 import 'react-native-gesture-handler';
 import React, {useState} from 'react';
-import {Button, View, Switch} from 'react-native';
+import {
+  Button,
+  View,
+  Switch,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
 import Geolocation from '@react-native-community/geolocation';
+//import { ScrollView } from 'react-native-gesture-handler';
 // Geolocation.setRNConfiguration(config);
 
 export default function App() {
   function HomeScreen() {
     return (
-      <View
+      <ScrollView
         style={{
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
           backgroundColor: isEnabled ? 'black' : 'white',
-        }}></View>
+        }}>
+        <View
+          style={{
+            backgroundColor: 'black',
+            width: '95%',
+            height: 150,
+            alignSelf: 'center',
+            marginTop: 10,
+          }}>
+          <Text
+            style={{
+              color: 'white',
+              marginTop: 30,
+              marginLeft: 10,
+              fontSize: 15,
+            }}>
+            Your balance is
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text
+              style={{
+                color: 'white',
+                marginLeft: 15,
+                fontSize: 15,
+                marginTop: 20,
+              }}>
+              Rs
+            </Text>
+            <Text style={{color: 'white', fontSize: 35, marginLeft: 10}}>
+              0
+            </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: 'red',
+              width: '90%',
+              height: 40,
+              alignSelf: 'center',
+              marginTop: 10,
+              top: 20,
+              borderRadius: 2,
+            }}>
+            <TouchableOpacity>
+              <Text
+                style={{
+                  color: 'white',
+                  alignSelf: 'center',
+                  fontWeight: 'bold',
+                  marginTop: 10,
+                }}>
+                TAP TO RECHARGE
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{width: '98%', alignItems: 'flex-end', marginTop: 30}}>
+          <TouchableOpacity>
+            <Text style={{color: 'red'}}>View more</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <View
+            style={{
+              alignItems: 'center',
+            }}>
+            <Text style={{fontWeight: 'bold'}}>DATA</Text>
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                borderColor: 'lightgray',
+                borderWidth: 3,
+                borderRadius: 50,
+              }}></View>
+            <Text style={{fontWeight: 'bold'}}>Per MB</Text>
+          </View>
+
+          <View
+            style={{
+              alignItems: 'center',
+            }}>
+            <Text style={{fontWeight: 'bold'}}>CALLS</Text>
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                borderColor: 'lightgray',
+                borderWidth: 3,
+                borderRadius: 50,
+              }}></View>
+            <Text style={{fontWeight: 'bold'}}>Per 60 s</Text>
+          </View>
+
+          <View
+            style={{
+              // borderColor: 'lightgray',
+              // borderWidth: 1,
+              alignItems: 'center',
+            }}>
+            <Text style={{fontWeight: 'bold'}}>SMS</Text>
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                borderColor: 'lightgray',
+                borderWidth: 3,
+                borderRadius: 50,
+              }}></View>
+            <Text style={{fontWeight: 'bold'}}>Per</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: '100%',
+            borderColor: 'lightgray',
+            borderWidth: 1,
+            marginTop: 20,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            marginTop: 30,
+          }}>
+          <View style={{marginTop: 50}}>
+            <Text>Packages</Text>
+          </View>
+
+          <View
+            style={{
+              borderColor: 'lightgray',
+              borderWidth: 1,
+              height: 80,
+            }}></View>
+          <View style={{marginTop: 50}}>
+            <Text>Daily Rewards</Text>
+          </View>
+
+          <View
+            style={{
+              borderColor: 'lightgray',
+              borderWidth: 1,
+              height: 80,
+            }}></View>
+
+          <View style={{marginTop: 50}}>
+            <Text>Make Your Bundle</Text>
+          </View>
+
+          <View
+            style={{
+              borderColor: 'lightgray',
+              borderWidth: 1,
+              height: 80,
+            }}></View>
+
+          <View style={{marginTop: 50}}>
+            <Text>More</Text>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
   function AboutScreen() {
@@ -46,7 +212,7 @@ export default function App() {
       <Drawer.Navigator
         screenOptions={{
           drawerStyle: {
-            backgroundColor: isEnabled ? 'darkgray' : 'white',
+            backgroundColor: isEnabled ? 'orange' : 'white',
           },
         }}
         initialRouteName="Home">
